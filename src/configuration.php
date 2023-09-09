156bf863c381b4f2d4a9af438e98fb074c9e98a7
le0m <mainardi.leo@gmail.com>
2023-09-09 03:34:17 +0200

    fix: array key undefined

--- src/configuration.php
+++ src/configuration.php
@@ -59,7 +59,7 @@ if ($op=="save"){
 		if ($session['user']['location'] == $settings['innname'])
 			$session['user']['location'] = stripslashes(httppost('innname'));
 	}
-	if (stripslashes(httppost("motditems")) != $settings['motditems']) {
+	if (stripslashes(httppost("motditems")) != ($settings['motditems'] ?? '')) {
 		invalidatedatacache("motd");
 	}
 	$post = httpallpost();
