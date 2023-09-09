2c3dbd7b502d192b6965da7e9168e1fa61335b95
le0m <mainardi.leo@gmail.com>
2023-09-09 02:43:58 +0200

    chore: add dbconnect example file

---
+++ src/dbconnect.example.php
@@ -0,0 +1,11 @@
+<?php
+//This file automatically created by installer.php on Sep 08, 2023 10:50 pm
+$DB_HOST = getenv("DB_HOST");
+$DB_PORT = getenv("DB_PORT") || 3306;
+$DB_USER = getenv("DB_USER");
+$DB_PASS = getenv("DB_PASS");
+$DB_NAME = getenv("DB_NAME");
+$DB_PREFIX = getenv("DB_PREFIX");
+$DB_USEDATACACHE = getenv("DB_USEDATACACHE");
+$DB_DATACACHEPATH = getenv("DB_DATACACHEPATH");
+?>
